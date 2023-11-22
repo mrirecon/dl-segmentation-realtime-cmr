f0234f66e6bf158db880b0e368980339603faab3
Martin Schilling <martin.schilling@med.uni-goettingen.de>
2023-11-22 10:09:22 +0100

    Two different conda environments

--- 01_scripts/assess_dl_conda_env.sh
+++ 01_scripts/assess_dl_conda_env.sh
@@ -9,11 +9,17 @@
 # To be able to activate conda environments,
 # run this script in interactive mode with bash -i assess_dl_conda_env.sh
 
-CONDA_ENV="assess_dl_seg"
-conda create -y -n $CONDA_ENV python=3.10.9 anaconda
+SCRIPT_REPO="$( cd "$( dirname "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_REPO"/01_scripts || exit
+
+read -ra param < <(grep -v '^#' assess_dl.env | xargs)
+export "${param[@]}"
+
+export NNUNET_DIR=$DATA_DIR/nnUNet
+
+conda create -y -n "$CONDA_ENV_EVAL" python=3.10.9 anaconda
 #required for reproducibility of figures
-conda run -n $CONDA_ENV conda install -y -c simpleitk simpleitk
-conda run -n $CONDA_ENV conda install -y -c conda-forge nibabel
+conda run -n "$CONDA_ENV_EVAL" conda install -y -c simpleitk simpleitk
+conda run -n "$CONDA_ENV_EVAL" conda install -y -c conda-forge nibabel
 #qt plugin installed within conda causes errors
-conda run -n $CONDA_ENV conda remove -y --force pyqt pyqt5-sip pyqtwebengine qt-main qt-webengine qtawesome qtconsole qtpy qtwebkit sphinxcontrib-qthelp
-#what else necessary for nnU-Net inference?
+conda run -n "$CONDA_ENV_EVAL" conda remove -y --force pyqt pyqt5-sip pyqtwebengine qt-main qt-webengine qtawesome qtconsole qtpy qtwebkit sphinxcontrib-qthelp
--- 01_scripts/assess_dl_nnunet_install.sh
+++ 01_scripts/assess_dl_nnunet_install.sh
@@ -17,13 +17,13 @@ export "${param[@]}"
 
 export NNUNET_DIR=$DATA_DIR/nnUNet
 
-conda create -n "$CONDA_ENV" -y python=3.10.9 anaconda
+conda create -n "$CONDA_ENV_NNUNET" -y python=3.10.9 anaconda
 
 #Install Pytorch with conda https://pytorch.org/get-started/locally/
-conda run -n "$CONDA_ENV" conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+conda run -n "$CONDA_ENV_NNUNET" conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 
 #Install nnUNet
-conda run -n "$CONDA_ENV" pip install nnunet
+conda run -n "$CONDA_ENV_NNUNET" pip install nnunet
 
 export nnUNet_raw_data_base="$NNUNET_DIR"
 export nnUNet_preprocessed="$NNUNET_DIR"
@@ -32,5 +32,5 @@ export RESULTS_FOLDER="$NNUNET_DIR"
 #---Check for pretrained weights---
 if ! [ -d "$NNUNET_DIR"/nnUNet/2d/Task027_ACDC ] ; then
 	echo "Downloading pretrained weights for Task027_ACDC"
-	conda run -n "$CONDA_ENV" nnUNet_download_pretrained_model Task027_ACDC
+	conda run -n "$CONDA_ENV_NNUNET" nnUNet_download_pretrained_model Task027_ACDC
 fi
